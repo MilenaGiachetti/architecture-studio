ffa2f839bdf7568e34f45af651f88ae11a148612
Milena Giachetti <giachettimilena@gmail.com>
2021-10-30 01:54:20 -0300

    Add proyects section & make header fixed

--- js/index.js
+++ js/index.js
@@ -17,3 +17,20 @@ const menuToggle = document.getElementById("menu_toggle");
 menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("menu__toggle--close");
 })
+
+// SCROLL SMOOTH for hash links
+const hashLinks = document.querySelectorAll("a[href*='#']");
+hashLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let hash = e.target.getAttribute("href");
+        let element = document.querySelector(hash);
+        let offset = element.offsetTop - 110;
+
+        window.scroll({ 
+            top: offset, 
+            behavior: "smooth" 
+        });
+    })
+})
