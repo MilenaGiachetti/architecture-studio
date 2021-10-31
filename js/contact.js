80806e362b80112ff8714c1f97844c63451975a3
Milena Giachetti <giachettimilena@gmail.com>
2021-10-31 11:09:26 -0300

    Add form validation

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,101 @@
+// message counter
 let message = document.getElementById("message_id");
 let counter = document.getElementById("message_counter");
 
 message.addEventListener("keyup", (e) => {
     let count = e.target.value.length;
     counter.textContent = count;
+})
+
+// --------------------------- VALIDATIONS --------------------------- //
+let form = document.getElementById("contact_form");
+let formBtn = document.getElementById("form_submit");
+let formElements = form.querySelectorAll("[required]")
+
+// remove form fields error mesagges
+formElements.forEach((el) => {
+    el.addEventListener("change", (e) => {
+        e.target.parentNode.classList.remove("invalid");
+    })
+    el.addEventListener("input", (e) => {
+        e.target.parentNode.classList.remove("invalid");
+    })
+})
+
+// individual validations
+const validateEmail = (email) => {
+    var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
+}
+
+const validateTel = (tel) => {
+    var m = tel.match(/\d/g); 
+    return m && m.length >= 5; // just check if it has at least 5 numbers - shortest private phone number
+}
+
+// field validation
+const validateFormField = (el) => {
+    var isValidField = true
+    if (el.tagName === "INPUT") {
+        switch (el.type) {
+            case 'text':
+                if (el.value == "") {
+                    el.parentNode.classList.add("invalid");
+                    isValidField = false;
+                }
+                break;
+            case 'email':
+                if (!validateEmail(el.value)) {
+                    el.parentNode.classList.add("invalid");
+                    isValidField = false; 
+                }
+                break;
+            case 'tel':
+                if (!validateTel(el.value)) {
+                    el.parentNode.classList.add("invalid");
+                    isValidField = false; 
+                }
+                break;
+        }
+    } else if (el.tagName === "SELECT") {
+        if (el.value == "") {
+            el.parentNode.classList.add("invalid");
+            isValidField = false;
+        }
+    } else if (el.tagName === "TEXTAREA") {
+        if (el.value == "") {
+            el.parentNode.classList.add("invalid");
+            isValidField = false; 
+        } 
+    }
+    return isValidField;
+}
+
+// form validation
+const formValidate = () => {
+	let isValidForm = true;
+    // Check each form field
+    for (let i = 0; i < formElements.length; i++) {
+        isValidForm = validateFormField(formElements[i]) && isValidForm;
+    }
+    return isValidForm; // return the valid status
+}
+
+// form submit handler
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!formValidate()) {
+        document.querySelector(".invalid [required]").focus();
+    } else {
+        console.log("Hola" +document.querySelector("#firstname_id").value + ", su consulta ya fue enviada! Nos contactaremos a la brevedad. Saludos!");
+    }
+})
+
+formBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!formValidate()) {
+        document.querySelector(".invalid [required]").focus();
+    } else {
+        console.log("Hola" +document.querySelector("#firstname_id").value + ", su consulta ya fue enviada! Nos contactaremos a la brevedad. Saludos!");
+    }
 })
