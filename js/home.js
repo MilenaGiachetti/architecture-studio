a48c8d36bfe07948b51eaa811327848ed9b3c398
Milena Giachetti <giachettimilena@gmail.com>
2021-11-01 14:18:05 -0300

    Add slider control btns & alert close aria label

--- js/home.js
+++ js/home.js
@@ -2,17 +2,20 @@
 const slides = document.getElementsByClassName("slide");
 const previousBtn = document.getElementById("slider-previous");
 const nextBtn = document.getElementById("slider-next");
+const btns = document.getElementsByClassName("slider__control");
 
 let currentSlideIndex = 0;
 
 const changeSlide = (index) => {
     slides[currentSlideIndex].classList.remove("slide--active");
+    btns[currentSlideIndex].classList.remove("slider__control--active");
     if (index < 0) {
         index = --slides.length;
     } else if (index === slides.length) {
         index = 0
     }
     slides[index].classList.add("slide--active");
+    btns[index].classList.add("slider__control--active");
     currentSlideIndex = index;
 }
 
@@ -23,3 +26,9 @@ previousBtn.addEventListener("click", () => {
 nextBtn.addEventListener("click", () => {
     changeSlide(currentSlideIndex + 1)
 })
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", (e) => {
+        changeSlide(+e.target.dataset.slide);
+    })
+}
